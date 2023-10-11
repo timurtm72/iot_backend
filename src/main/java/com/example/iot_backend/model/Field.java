bb5eff4b20f55c521cc50cd3571903bee6526a50
timurtm72 <timur.sultanov1972@gmail.com>
2023-10-11 16:44:48 +0300

    add user,sensor,device,field class

--- src/main/java/com/example/iot_backend/model/Field.java
+++ src/main/java/com/example/iot_backend/model/Field.java
@@ -1,5 +1,6 @@
 package com.example.iot_backend.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -8,9 +9,14 @@ import org.hibernate.annotations.Where;
 
 @Data
 @Entity
-@Table(name="filed")
+@Table(name="field")
 @SQLDelete(sql = "UPDATE field SET is_removed = true WHERE id = ?")
 @Where(clause = "is_removed=false")
 public class Field extends AbstractEntity{
-    
+    @Column(name = "float_value")
+    private float floatValue;
+    @Column(name = "long_value")
+    private long longValue;
+    @Column(name = "bool_value")
+    private boolean boolValue;
 }
