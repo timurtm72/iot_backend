ddb9466c8b2ae7d5c9aaa58702339e10d5be8790
timurtm72 <timur.sultanov1972@gmail.com>
2024-06-07 13:57:57 +0300

    add lombok annotations

--- src/main/java/com/example/iot_backend/model/Base/RegulatorDeviceBase.java
+++ src/main/java/com/example/iot_backend/model/Base/RegulatorDeviceBase.java
@@ -6,32 +6,32 @@ import lombok.*;
 
 import java.io.Serializable;
 
-//@MappedSuperclass
-//@EqualsAndHashCode
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Getter
-//@Setter
-//@ToString
+@MappedSuperclass
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public abstract class RegulatorDeviceBase  implements Serializable {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    @Column(name = "reg_mode")
-//    @Enumerated(EnumType.STRING)
-//    private RegMode regMode;
-//    @Column(name = "set_value", nullable = false)
-//    private Float setValue;
-//
-//    @Column(name = "hysteresis", nullable = false)
-//    private Float hysteresis;
-//
-//    @Column(name = "name", nullable = false)
-//    private String name;
-//
-//    @Column(name = "description", nullable = false)
-//    private String description;
-//
-//    @Column(name = "value_name", nullable = false)
-//    private String valueName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "reg_mode")
+    @Enumerated(EnumType.STRING)
+    private RegMode regMode;
+    @Column(name = "set_value", nullable = false)
+    private Float setValue;
+
+    @Column(name = "hysteresis", nullable = false)
+    private Float hysteresis;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "value_name", nullable = false)
+    private String valueName;
 }
